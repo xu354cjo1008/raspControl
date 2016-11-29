6a13ad6594d3cdf573afc09f0c01fff4b709158a
ChingChang Li <xu354cjo1008@gmail.com>
2016-11-29 05:52:25 +0000

    Interact with user input from console

--- client.c
+++ client.c
@@ -12,10 +12,13 @@
 int runClient(char *address, int port)
 {
     int sockfd = 0, n = 0;
+    char str[100];
     char recvBuff[1024];
+    char sendBuff[1024];
     struct sockaddr_in serv_addr;
 
     memset(recvBuff, '0',sizeof(recvBuff));
+    memset(sendBuff, '0',sizeof(sendBuff));
     if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("\n Error : Could not create socket \n");
@@ -33,26 +36,22 @@ int runClient(char *address, int port)
         return 1;
     }
 
-    printf("connect to remote: %s:%d\n", address, port);
-
     if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0 )
     {
         printf("\n Error : Connect Failed \n");
         return 1;
     }
 
-    while ( (n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0 )
-    {
-        recvBuff[n] = 0;
-        if(fputs(recvBuff, stdout) == EOF)
-        {
-            printf("\n Error : Fputs error\n");
-        }
-    }
+    printf("connect to remote: %s:%d\n", address, port);
 
-    if(n < 0)
-    {
-        printf("\n Read error \n");
+    while(1) {
+        printf( "Enter a value :" );
+        scanf("%s", sendBuff);
+        printf( "Entered : %s\n", sendBuff);
+        if (strncmp(sendBuff, "exit", sizeof("exit")) == 0) {
+            break;
+        }
+        write(sockfd, sendBuff, sizeof(sendBuff));
     }
 
     return 0;
--- server.c
+++ server.c
@@ -1,6 +1,5 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,14 +8,49 @@
 #include <sys/types.h>
 #include <time.h>
 
+typedef struct {
+    int *array;
+    size_t used;
+    size_t size;
+} connArray;
+
+void initConnArray(connArray *arr, size_t initSize)
+{
+    arr->array = (int *)malloc(initSize * sizeof(int));
+    arr->used = 0;
+    arr->size = initSize;
+}
+
+void connArrayPush(connArray *arr, int conn)
+{
+    if (arr->used == arr->size) {
+        arr->size *= 2;
+        arr->array = (int *)malloc(arr->size * sizeof(int));
+    }
+    arr->array[arr->used++] = conn;
+}
+
+void freeConnArray(connArray *arr)
+{
+    if (arr->array) {
+        free(arr->array);
+        arr->array = NULL;
+    }
+    arr->size = 0;
+    arr->used = 0;
+}
+
 int runServer(int port)
 {
 
     int listenfd = 0, connfd = 0;
+    int n = 0;
     struct sockaddr_in serv_addr;
 
     char sendBuff[1025];
+    char recvBuff[1025];
     time_t ticks;
+    connArray conns;
 
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
     memset(&serv_addr, '0', sizeof(serv_addr));
@@ -32,13 +66,23 @@ int runServer(int port)
 
     listen(listenfd, 10);
 
+    initConnArray(&conns, 10);
+
     while(1)
     {
         connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
 
-        ticks = time(NULL);
-        snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
-        write(connfd, sendBuff, strlen(sendBuff));
+        printf("accept connection: %d\n", connfd);
+
+        while ( (n = read(connfd, recvBuff, sizeof(recvBuff)-1)) > 0 )
+        {
+            recvBuff[n] = 0;
+            printf("get: %s\n", recvBuff);
+//            if(fputs(recvBuff, stdout) == EOF)
+//            {
+//                printf("\n Error : Fputs error\n");
+//            }
+        }
 
         close(connfd);
         sleep(1);
